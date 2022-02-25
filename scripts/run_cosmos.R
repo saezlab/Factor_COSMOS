20eb6c60d5a1055b0fbced47de0f91c1bcb6a336
adugourd <aurelien.dugourd@bioquant.uni-heidelberg.de>
2022-02-25 13:37:52 +0100

    added cbc and updated practical slides

--- scripts/run_cosmos.R
+++ scripts/run_cosmos.R
@@ -1,12 +1,13 @@
 library(cosmosR)
 library(readr)
+library(dplyr)
 
 data("meta_network")
 load("data/cosmos/cosmos_inputs.RData")
 
 names(cosmos_inputs)
 
-cell_line <- "COLO 205"
+cell_line <- "786-0"
 
 sig_input <- cosmos_inputs[[cell_line]]$TF_scores
 metab_input <- cosmos_inputs[[cell_line]]$metabolomic
@@ -16,9 +17,10 @@ compartment_code <- "_c"
 
 names(metab_input) <- paste(names(metab_input),compartment_code, sep = "")
 names(metab_input) <- paste("Metab__",names(metab_input),sep = "")
+
 ##Filter sugnificant inputs
 sig_input <- sig_input[abs(sig_input) > 2]
-metab_input <- metab_input[abs(metab_input) > 1.5]
+metab_input <- metab_input[abs(metab_input) > 2]
 
 #In order to adapt options to users specification we can load them into a variable 
 #that will then be passed to preprocess_COSMOS_signaling_to_metabolism CARNIVAL_options parameter
@@ -26,7 +28,7 @@ my_options <- default_CARNIVAL_options()
 
 #Here the user should provide a path to its CPLEX executable (only cplex at the moment, other solvers will be documented soon !)
 # my_options$solverPath <- "~/Documents/cplex" #or cbc solver executable
-my_options$solverPath <- "~/Documents/cbc-osx/cbc" #or cbc solver executable
+my_options$solverPath <- "cbc/cbc-osx/cbc" #or cbc solver executable
 # my_options$solver <- "cplex" #or cbc
 my_options$solver <- "cbc"
 my_options$timelimit <- 1800
@@ -62,4 +64,53 @@ ATT <- merge(ATT, RNA_input_df, all.x = T)
 
 write_csv(SIF, file = paste("results/",paste(cell_line, "_SIF.csv",sep = ""), sep = ""))
 write_csv(ATT, file = paste("results/",paste(cell_line, "_ATT.csv",sep = ""), sep = ""))
+
+my_options$timelimit <- 1800
+
+test_back <- preprocess_COSMOS_metabolism_to_signaling(meta_network = meta_network,
+                                                      signaling_data = sig_input,
+                                                      metabolic_data = metab_input,
+                                                      diff_expression_data = RNA_input,
+                                                      maximum_network_depth = 4,
+                                                      remove_unexpressed_nodes = T,
+                                                      filter_tf_gene_interaction_by_optimization = T,
+                                                      CARNIVAL_options = my_options
+                                                      
+)
+
+my_options$timelimit <- 7200
+
+test_result_back <- run_COSMOS_metabolism_to_signaling(data = test_back,
+                                                      CARNIVAL_options = my_options)
+
+formatted_res_back <- format_COSMOS_res(test_result_back)
+
+SIF_back <- formatted_res_back[[1]]
+ATT_back <- formatted_res_back[[2]]
+
+ATT_back <- merge(ATT_back, RNA_input_df, all.x = T)
+
+write_csv(SIF_back, file = paste("results/",paste(cell_line, "_SIF_back.csv",sep = ""), sep = ""))
+write_csv(ATT_back, file = paste("results/",paste(cell_line, "_ATT_back.csv",sep = ""), sep = ""))
+
+SIF_full <- as.data.frame(rbind(SIF,SIF_back))
+SIF_full <- unique(SIF_full)
+
+ATT_full <- as.data.frame(rbind(ATT,ATT_back))
+ATT_full <- unique(ATT_full)
+
+S_nodes <- ATT_full[ATT_full$NodeType == "S","Nodes"]
+T_nodes <- ATT_full[ATT_full$NodeType == "T","Nodes"]
+C_nodes <- union(S_nodes,T_nodes)
+# C_nodes <- C_nodes[which(C_nodes %in% c(SIF$Node1,SIF$Node2) & C_nodes %in% c(SIF_back$Node1,SIF_back$Node2))]
+
+ATT_full <- ATT_full[,-6]
+
+ATT_full <- ATT_full %>% group_by(Nodes) %>% summarise_each(funs(mean(., na.rm = TRUE)))
+ATT_full <- as.data.frame(ATT_full)
+
+ATT_full$NodeType <- ifelse(ATT_full$Nodes %in% C_nodes,"C",ifelse(ATT_full$Nodes %in% S_nodes,"S",ifelse(ATT_full$Nodes %in% T_nodes,"T","")))
+
+write_csv(SIF_full, file = paste("results/",paste(cell_line, "_SIF_full.csv",sep = ""), sep = ""))
+write_csv(ATT_full, file = paste("results/",paste(cell_line, "_ATT_full.csv",sep = ""), sep = ""))
 
